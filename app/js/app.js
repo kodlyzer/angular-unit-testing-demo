669188dba4cdac25d88f2937f66606ccbe1680e2
karthik Rana <rana3012@gmail.com>
2016-01-28 21:26:32 +0530

    sets up trip app with city country adding feature

--- app/js/app.js
+++ app/js/app.js
@@ -4,4 +4,18 @@ angular.module('testingAngularApp')
 
 .controller('testingAngularCtrl', ['$rootScope', '$scope', function($rootScope, $scope){
 	$scope.title = "Testing Angular Applications";
+
+	$scope.destinations = [];
+
+	$scope.newDestination = {
+		city: undefined,
+		country: undefined
+	};
+
+	$scope.addDestination = function () {
+		$scope.destinations.push({
+			city: $scope.newDestination.city,
+			country: $scope.newDestination.country
+		});
+	};
 }])
--- test/unit/testingAngularUnitSpec.js
+++ test/unit/testingAngularUnitSpec.js
@@ -1,16 +1,45 @@
-describe('Testing AngularJS Test Suite', function () {
-	describe('Testing AngularJS Controller', function () {
-		it('should initialize the title in the scope', function () {
-			module('testingAngularApp');
+describe('Testing AngularJS Test Suite', function() {
+    beforeEach(module('testingAngularApp'));
 
-			var scope = {},
-				ctrl;
-			inject(function($controller) {
-				ctrl = $controller('testingAngularCtrl', {$scope:scope});
-			});
+    describe('Testing AngularJS Controller', function() {
+    	var scope, ctrl;
+        beforeEach(inject(function($controller, $rootScope) {
+        	scope = $rootScope.$new();
+            ctrl = $controller('testingAngularCtrl', {
+                $scope: scope
+            });
+        }));
 
-			expect(scope.title).toBeDefined();
-			expect(scope.title).toBe("Testing Angular Applications");
-		});
-	});
+        it('should initialize the title in the scope', function() {
+            expect(scope.title).toBeDefined();
+            expect(scope.title).toBe("Testing Angular Applications");
+        });
+
+        it('should add destination city and country to the destinations list', function () {
+        	expect(scope.destinations).toBeDefined();
+        	expect(scope.destinations.length).toBe(0);
+
+        	scope.newDestination = {
+        		city: "Trivandrum",
+        		country: "India"
+        	};
+
+        	scope.addDestination();
+
+        	expect(scope.destinations.length).toBe(1);
+        	expect(scope.destinations[0].city).toBe("Trivandrum");
+        	expect(scope.destinations[0].country).toBe("India");
+
+        	scope.newDestination.city = "London";
+        	scope.newDestination.country = "England";
+
+        	scope.addDestination();
+
+        	expect(scope.destinations.length).toBe(2);
+        	expect(scope.destinations[1].city).toBe("London");
+        	expect(scope.destinations[1].country).toBe("England");
+        	expect(scope.destinations[0].city).toBe("Trivandrum");
+        	expect(scope.destinations[0].country).toBe("India");
+        });
+    });
 });
